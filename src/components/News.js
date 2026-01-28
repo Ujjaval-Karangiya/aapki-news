4daf4d3719a8d70c953f6e72372bcb81055772be
Ujjaval-Karangiya <ujjavalk2401@gmail.com>
2026-01-28 21:08:36 +0530

    render2

--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props)=>{
+const News = (props) => {
     const [articles, setArticles] = useState([])
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
-    
+
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
-    } 
-
-   const updateNews = async () => {
+    }
+const updateNews = async () => {
   try {
     props.setProgress(10);
     setLoading(true);
 
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://gnewsapi.io/api/v4/top-headlines?country=${props.country}&topic=${props.category}&token=${props.apiKey}&page=${page}&max=${props.pageSize}`;
 
     const response = await fetch(url);
     props.setProgress(30);
@@ -33,8 +32,6 @@ const News = (props)=>{
     props.setProgress(70);
 
     setArticles(parsedData.articles || []);
-    setTotalResults(parsedData.totalResults || 0);
-
     props.setProgress(100);
   } catch (error) {
     console.error("Error fetching news:", error);
@@ -43,18 +40,19 @@ const News = (props)=>{
   }
 };
 
+
     useEffect(() => {
         document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
-        updateNews(); 
+        updateNews();
         // eslint-disable-next-line
     }, [])
 
 
-const fetchMoreData = async () => {
+   const fetchMoreData = async () => {
   try {
     const nextPage = page + 1;
 
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${nextPage}&pageSize=${props.pageSize}`;
+    const url = `https://gnewsapi.io/api/v4/top-headlines?country=${props.country}&topic=${props.category}&token=${props.apiKey}&page=${nextPage}&max=${props.pageSize}`;
 
     setPage(nextPage);
 
@@ -68,35 +66,41 @@ const fetchMoreData = async () => {
     setArticles((prevArticles) =>
       prevArticles.concat(parsedData.articles || [])
     );
-
-    setTotalResults(parsedData.totalResults || 0);
   } catch (error) {
     console.error("Error fetching more news:", error);
   }
 };
 
- 
-        return (
-            <>
-                <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>aapki - news - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-                {loading && <Spinner />}
-                <InfiniteScroll
+
+
+    return (
+        <>
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>aapki - news - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            {loading && <Spinner />}
+            {/* <InfiniteScroll
                     dataLength={articles.length}
                     next={fetchMoreData}
                     hasMore={articles.length !== totalResults}
                     loader={<Spinner/>}
-                > 
-                    <div className="container">
-                         
-                    <div className="row">
-                        {articles.map((element) => {
-                            return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-                           </div>
-                        })}
-                    </div>
-                    </div> 
-                </InfiniteScroll>
+                >  */}
+            <InfiniteScroll
+                dataLength={articles.length}
+                next={fetchMoreData}
+                hasMore={articles.length < 100}
+                loader={<Spinner />}
+            />
+
+            <div className="container">
+
+                <div className="row">
+                    {articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                        </div>
+                    })}
+                </div>
+            </div>
+        </InfiniteScroll >
             </>
         )
     
